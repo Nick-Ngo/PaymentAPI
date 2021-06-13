f2e1a839e09a0c047032b6b6a8a1599bdfc50b7a
Hieu <hieu.n2395@gmail.com>
2021-06-13 09:59:02 +0700

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    [Table("PaymentDetails")]
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "NVARCHAR(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "NVARCHAR(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "NVARCHAR(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
